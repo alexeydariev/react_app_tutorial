8c7e10ad01343302b46ef8ca2a88160a7fbdf733
alexeydariev <alexeydariev@gmail.com>
2019-04-08 21:01:59 +0300

    adding frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {
+  Container,
+  Navbar,
+  NavbarBrand,
+  Row,
+  Jumbotron,
+  InputGroup,
+  InputGroupAddon,
+  Button,
+  Input,
+  Col
+} from 'reactstrap';
+
+import Weather from './Weather';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      weather: null,
+      cityList:[],
+      newCityName: ''
+    };
+  }
+
+getCityList = () => {
+  fetch('/api/cities')
+  .then(res => res.json())
+  .then(res =>{
+    var cityList = res.map(r => r.city_name);
+    this.setState({cityList});
+  });
+};
+
+handleInputChange = (e) => {
+  this.setState({newCityName: e.target.value});
+}
+
+componentDidMount () {
+  this.getCityList();
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container fluid className = "centered">
+        <Navbar dark color ="dark">
+          <NavbarBrand href="/"> WeatherApplication</NavbarBrand>
+        </Navbar>
+      <Row>
+          <Col>
+          <Jumbotron>
+              <h1 className="display-3">Look how cold it is!</h1>
+              <p className="lead">Weather from city you select</p>
+                <InputGroup>
+            <Input
+              placeholder="City name goes here..."
+              value = {this.state.newCityName}
+              onChange= {this.handleInputChange}
+            />
+            <InputGroupAddon addonType ="append">
+              <Button color="primary" onClick= {this.handleAddCity}>Add</Button>
+            </InputGroupAddon>
+            </InputGroup>
+          </Jumbotron>
+          </Col>
+      </Row>
+      <Row>
+          <Col></Col>
+      </Row>
+      <Weather />
+      </Container>
     );
   }
 }
